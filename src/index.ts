1f542520c2cb1ad2c199739ac08e381bef6097b2
Jasper Stephenson <jasperstephenson@gmail.com>
2024-09-12 14:57:12 +0900

    sub

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // share docs with id-lix@lix-338122.iam.gserviceaccount.com
 
 const PORT = process.env.PORT || 3047
+const BASE_URL = '/3lix'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -17,21 +18,26 @@ import cors from 'cors'
 const app = express()
 app.use(cors())
 
-app.get('/', (req, res) => {
+const router = express.Router()
+
+router.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.get('/debug6969/files', async (req, res) => {
+router.get('/debug6969/files', async (req, res) => {
   c.log('Getting all files')
   res.send(await files())
 })
 
-app.get('/debug6969/reloadallfiles', async (req, res) => {
-  c.log('Reloading all files')
-  res.send(await reloadAllFiles())
-})
+router.get(
+  '/debug6969/reloadallfiles',
+  async (req, res) => {
+    c.log('Reloading all files')
+    res.send(await reloadAllFiles())
+  },
+)
 
-app.get('/doc/:id', async (req, res) => {
+router.get('/doc/:id', async (req, res) => {
   const id = req.params.id
 
   const html = await getHtml(id)
@@ -41,6 +47,8 @@ app.get('/doc/:id', async (req, res) => {
   }
   res.send(html)
 })
+
+app.use(BASE_URL, router)
 
 app.listen(PORT, () => {
   c.log('Express listening on port', PORT)
